bd59821249d0dccd9338d0c16f7083424b3099e8
Rob Dale <email": null>
2016-09-16 15:04:49 -0500

    iconified the spot icons for items pulled from the database

--- app/assets/javascripts/loadMarkers.js
+++ app/assets/javascripts/loadMarkers.js
@@ -14,11 +14,37 @@ function loadMarkers(){
       success: function(response){
         for (var i = 0; i < response.length; i++) {
 
+      var image;
+      switch (response[i].spot_type) {
+        case "shelter":
+          image = "http://jellyfishpi.com/images/shelterPin.png";
+          break;
+        case "food":
+          image = "http://jellyfishpi.com/images/foodPin.png";
+          break;
+        case "restroom":
+          image = "http://jellyfishpi.com/images/shelterPin.png";
+          break;
+        case "wifi":
+          image = "http://jellyfishpi.com/images/wifiPin.png";
+          break;
+        case "charging":
+          image = "http://jellyfishpi.com/images/chargingPin.png";
+          break;
+        case "":
+          image = "http://jellyfishpi.com/images/healthPin.png";
+          break;
+        default:
+          image = "http://jellyfishpi.com/images/shelterPin.png";
+          break;
+      }
+
         var home = {lat: parseFloat(response[i].latitude), lng: parseFloat(response[i].longitude)};
         var marker = new google.maps.Marker({
             position: home,
             map: map,
-            title: response[i].name
+            title: response[i].name,
+            icon: image
           });
 
         }
