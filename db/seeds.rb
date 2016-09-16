6645c6636f3c131f7750bc22b611b9dcb5381b37
Rob Dale <email": null>
2016-09-16 13:58:38 -0500

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Spot.create(name: "Assumption Catholic Church", spot_type: "shelter", upvotes: 2, latitude: 41.8904969, longitude: -87.6364649)
+Spot.create(name: "3 Greens", spot_type: "restroom", upvotes: 2, latitude: 41.8900383, longitude: -87.6375984)
+Spot.create(name: "Starbucks", spot_type: "wifi", upvotes: 2, latitude: 41.8897676, longitude: -87.6371599)
+Spot.create(name: "Shivalya Hindu Cultural Center", spot_type: "food", upvotes: 2, latitude: 41.8916779, longitude: -87.6363554)
+Spot.create(name: "Alliance of Chicago Community Health Services", spot_type: "health", upvotes: 2, latitude: 41.8921745, longitude: -87.6349667)
